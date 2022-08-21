06d60d8fcfc748614504c2f0d04e684f5b5f0c8d
ndNanda <96295151+ndNanda@users.noreply.github.com>
2022-08-21 00:07:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("EnterUsername");
+ var b=prompt("EnterPassword");
+  	if(a== "Naba" && b=="22-08")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
